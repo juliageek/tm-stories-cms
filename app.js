436fc1a6a15da85777ed0b2a892d693d450de604
juliageek <iulia.mihet@gmail.com>
2021-02-27 20:57:51 +0200

    Second commit

--- app.js
+++ app.js
@@ -7,20 +7,31 @@ const locales = [
   },
   {
     value: 'en',
-    label: 'United States'
+    label: 'English (US)'
   }
 ]
 
-const defaultLocale = 'en';
+const defaultLocale = 'ro';
 
 var apos = require('apostrophe')({
   shortName: 'tm-stories-cms',
   modules: {
+    settings: {
+      alias: 'settings',
+      locales: locales.map(locale => ({
+        label: locale.label,
+        name: locale.value,
+        _url:  `/modules/apostrophe-workflow/link-to-locale?slug=2%F&locale=${locale.value}`
+      }))
+    },
     'apostrophe-i18n-static': {
       defaultLocale,
       locales,
       namespaces: true,
-      ignoreNamespaces: [ 'apostrophe' ]
+      ignoreNamespaces: [ 'apostrophe' ],
+      generateAtStartup: false,
+      objectNotation: true,
+      autoReload: false
     },
     'apostrophe-images': {
       enableAltField: true
@@ -31,8 +42,10 @@ var apos = require('apostrophe')({
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, '/views') },
     'apostrophe-workflow': {
       alias: 'workflow',
-      // Recommended to save database space. You can still
-      // export explicitly between locales
+      prefixes: {
+        'en': '/en',
+        'ro': '/ro'
+      },
       locales: [{
         name: 'default',
         label: 'Default',
@@ -48,9 +61,11 @@ var apos = require('apostrophe')({
     'my-theme': {},
     'antiquity': { extend: 'apostrophe-blog' },
     'antiquity-pages': { extend: 'apostrophe-pieces-pages' },
+    'antiquity-widgets': {},
     'medieval': { extend: 'apostrophe-blog' },
     'medieval-pages': { extend: 'apostrophe-pieces-pages' },
     // Content widgets
-    'two-panel-widgets': {}
+    'two-panel-widgets': {},
+    'three-panel-widgets': {}
   }
 });
